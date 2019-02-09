bcc44e7e1c76c3eb08277562a565c8e8d4727ea8
Paras Gupta <parasg1999@gmail.com>
2019-02-09 14:35:05 +0530

    Story posted

--- routes/writing.js
+++ routes/writing.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var router = express.Router();
 var bodyParser = require('body-parser');
-// router.use(bodyParser.urlencoded({extended: true}));
-// router.use(bodyParser.json());
 
-var jsonParser = bodyParser.json({ type: 'application/json'});
+var Node = require('../models/node');
+
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
+
+// var jsonParser = bodyParser.json({ type: 'application/json'});
 
 router.get("/", function(req, res) {
     res.render('index');
@@ -14,8 +17,30 @@ router.get("/postStory", function(req, res) {
     res.render('postStory');
 });
 
-router.post("/post", jsonParser, function(req, res) {
-    console.log("sfsf" ,req.body);
+router.post("/post", function(req, res) {
+    // console.log(JSON.stringify(req.body));
+
+    var nodesArr = req.body.nodes;
+    nodesArr.forEach(element => {
+        var newNode = new Node({
+            content: element.content,
+            end: element.end,
+        });
+        
+        if(element.end === 'false') {
+            newNode.question.content = element.question;
+            
+            element.options.forEach(option => {
+                var optionObject = {};
+                optionObject.key = option;
+                newNode.question.options.push(optionObject);
+            });
+        }
+        
+
+        newNode.save();
+    });
+
 });
 
 router.get("/postStory", function(req, res) {
