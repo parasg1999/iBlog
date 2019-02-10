0fd84cab154c34aab4318ac3901c806f5ddca6f2
Paras Gupta <parasg1999@gmail.com>
2019-02-10 16:56:30 +0530

    PROJECT DONE

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res) {
-  res.render('index');
-
+  res.redirect('/reading');
 });
+
+// router.get("/contact", function (req, res) {
+//   res.redirect('/c');
+// });
 
 module.exports = router;
